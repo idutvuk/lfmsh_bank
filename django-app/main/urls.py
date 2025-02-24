960af858fddb8c148b57256b5daf2a0da62bb3ec
idutvuk <idutvuk@gmail.com>
2025-02-24 14:21:39 +0100

    empty url redirect

--- django-app/main/urls.py
+++ django-app/main/urls.py
@@ -1,11 +1,11 @@
 from django.urls import re_path as url
 from django.conf.urls import  include
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', RedirectView.as_view(url='/bank/')),
     url(r'^admin/', admin.site.urls),
     url(r'^bank/', include('bank.urls', namespace='bank')),
     url(r'^bank_api/', include('bank_api.urls', namespace='bank_api'))
